3cf3d01cd1b4c805b9d0667b63b512d152ef293a
elijor <ejg96@drexel.edu>
2023-10-09 11:14:17 -0400

    Initial commit

---
+++ final/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>IDM232-ejg96</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+  <?php include_once "header.php";?>
+  <?php 
+    $var1 = "Assignment - 01";
+    ?>
+    <h1>
+    <?php 
+    echo $var1;
+    ?>
+    </h1>
+    <?php include_once "footer.php";?>
+  </body>
+</html>
