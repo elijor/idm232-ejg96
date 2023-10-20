f5bec88e58e1156462f151145f84cb414a54092c
elijor <ejg96@drexel.edu>
2023-10-19 23:51:17 -0400

    styling update

---
+++ hwk01/script.js
@@ -0,0 +1,11 @@
+const checkbox = document.querySelectorAll(".cb");
+const labelcolor = document.querySelectorAll("label");
+
+checkbox.addEventListener("change") {
+  if (this.checked) {
+    console.log("testing ugh");
+    labelcolor[index].style.color = "#ff0000"; // Change color when the checkbox is checked
+  } else {
+    labelcolor[index].style.color = "#ea5624"; // Restore the default color when unchecked
+  }
+};
